37bd1e117b329a8304a094b5cc377292c9f74649
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-11-24 18:58:07 +0530

    Added : Pattern to validate last name

--- com/userregistration/UserRegistration.java
+++ com/userregistration/UserRegistration.java
@@ -9,12 +9,14 @@ public class UserRegistration {
 	static int loop = 0; 
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration process");
+		System.out.println("********* Welcome to User Registration process **********");
 		
 		validationProcess.firstName();
+		validationProcess.lastName();
 	}
 	
 	public void firstName() {
+		
 		while (loop == 0) {
 			System.out.println("\nEnter your valid first name starting with capital letter and minimum 3 Character");
 			String firstName = scanner.nextLine();
@@ -22,11 +24,29 @@ public class UserRegistration {
 			
 			if (firstName.matches(firstNamePattern)) {
 				System.out.println("First name is valid");
-				loop = 1;	
+				break;	
 			}
 			else 
 				System.out.println("First name is invalid"); 
-		}
+		}	
+		System.out.println("---------------------------------");
+	}
+	
+	public void lastName() {
 		
-	}		
+		while (loop == 0) {
+			System.out.println("\nEnter your valid Last name starting with capital letter and minimum 3 Character");
+			String firstName = scanner.nextLine();
+			String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+			
+			if (firstName.matches(firstNamePattern)) {
+				System.out.println("Last name is valid");
+				break;
+			}
+			else 
+				System.out.println("Last name is invalid"); 
+		}
+		System.out.println("---------------------------------");
+	}	
+	
 }
